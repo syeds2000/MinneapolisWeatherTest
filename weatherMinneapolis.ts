a5d8191513edef3ff303808efc69a0f3f467b790
Syed Salim <syeds2000@gmail.com>
2019-09-27 10:18:51 -0500

    test file calling the function

---
+++ weatherMinneapolis.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by ssalim on 9/27/2019.
+ */
+
+import {WeatherFunc} from "../funcLib/weatherFunc";
+
+describe('revel test ', () => {
+    it('should print best outreach method with date', () => {
+        WeatherFunc.printBestOutreachMethodWithDate();
+    });
+});
